043e82a28feda3537026d902a5255e7a27e0ed1f
ganesh <sindhu91*>
2021-08-22 14:17:48 +0530

    commit initial object model structure

---
+++ src/main/java/com/singtel/assignment/AssignmentApplication.java
@@ -0,0 +1,17 @@
+package com.singtel.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author Ganesh T
+ * Spring boot application
+ */
+@SpringBootApplication
+public class AssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentApplication.class, args);
+    }
+
+}
